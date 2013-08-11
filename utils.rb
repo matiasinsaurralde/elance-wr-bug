f4e90858bcd9d881afa29093ee01b9de47a70725
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-08-11 12:35:37 -0400

    simple #email_exists?, returns boolean

--- utils.rb
+++ utils.rb
@@ -6,14 +6,23 @@ require 'open-uri'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 module Elance
-	def check_email_existance( input_email )
+	def email_exists?( input_email )
 
 		# get a valid token
 		login_page = open('https://www.elance.com/php/landing/main/login.php').read()
 		token = Nokogiri::HTML( login_page ).css('#token').attr('value').value()
 
 		# try authentication
-		p RestClient.post( 'https://www.elance.com/php/reg/main/signInAHR.php?t=1376232800475',
-				 { :token => token, :mode => 'signin', :crypted => '', :login_type => '', :redirect => '', :lnm => 'test@lolo.com', :pwd => '123123' } )
+		response =  RestClient.post( 'https://www.elance.com/php/reg/main/signInAHR.php?t=1376232800475',
+				 { :token => token,
+				   :mode => 'signin',
+				   :crypted => '',
+				   :login_type => '',
+				   :redirect => '',
+				   :lnm => input_email,
+				   :pwd => '12345' } )
+
+		return response.include?('do not match')
+
 	end
 end
